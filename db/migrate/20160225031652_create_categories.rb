dc2385702029f46d03ed7c290abde57f15fc7117
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-02-24 20:17:12 -0700

    adds categories models

---
+++ db/migrate/20160225031652_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.text :name
+      t.text :description
+      t.boolean :is_active
+
+      t.timestamps null: false
+    end
+  end
+end
